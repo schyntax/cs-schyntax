e5ec5b3006a4ab6bf402801870a85658f95f8d66
Bret Copeland <bret@atlantisflight.org>
2015-03-28 18:18:57 -0400

    Setup AppVeyor.

--- Schyntax/Properties/AssemblyInfo.cs
+++ Schyntax/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Schyntax")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library and DSL for defining scheduled tasks.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Schyntax")]
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
