7818426b826ff1e60f4433a72491792faff567e4
Bret Copeland <bret@atlantisflight.org>
2015-07-19 19:33:07 -0400

    Clean up nuspec for v1.0.

--- Schyntax/Properties/AssemblyInfo.cs
+++ Schyntax/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Schyntax")]
-[assembly: AssemblyDescription("A library and DSL for defining scheduled tasks.")]
+[assembly: AssemblyDescription("A library and DSL for defining schedules.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Schyntax")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
