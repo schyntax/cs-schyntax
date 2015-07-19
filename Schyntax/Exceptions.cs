650a0fa3967eeb9f5e3207c6d3a769197ce2f2a4
Bret Copeland <bret@atlantisflight.org>
2015-07-19 18:59:35 -0400

    Remove Schtick. It's moving to its own library.

--- Schyntax/Exceptions.cs
+++ Schyntax/Exceptions.cs
@@ -51,16 +51,4 @@ namespace Schyntax
             Data["Schedule"] = schedule;
         }
     }
-
-    public sealed class ScheduleCrashException : SchyntaxException
-    {
-        public ScheduledTask Task { get; }
-
-        internal ScheduleCrashException(string message, ScheduledTask task, Exception innerException) : base(message, innerException)
-        {
-            Task = task;
-            Data["TaskName"] = task.Name;
-            Data["Schedule"] = task.Schedule.OriginalText;
-        }
-    }
 }
